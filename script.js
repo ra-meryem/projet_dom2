74fe30aaee685ea2d32ac2b0eea1a3fc58d15506
meryem raji <meryemr420@gmail.com>
2024-12-18 13:45:40 +0100

    initialiser le projet

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const colorBox = document.getElementById('color-box');
+    const changeColorBtn = document.getElementById('change-color-btn');
+  
+    // Fonction pour générer une couleur aléatoire
+    function getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+  
+    // Écouteur d'événement sur le bouton
+    changeColorBtn.addEventListener('click', function() {
+      const randomColor = getRandomColor();
+      colorBox.style.backgroundColor = randomColor;
+    });
+  });
+  
